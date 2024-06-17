5a166d055042f8f603eb77e36bd62dbfb6a24924
Ari Abrams <ariabrams65@gmail.com>
2024-06-16 19:47:01 -0700

    added oil + other changes

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -54,6 +54,16 @@ return {
                     vim.lsp.buf.clear_references()
                 end
             })
+
+            -- border for hover windows
+            vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
+                vim.lsp.handlers.hover,
+                {border = 'rounded'}
+            )
+            vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
+                vim.lsp.handlers.signature_help,
+                {border = 'rounded'}
+            )
         end
     }
 }
